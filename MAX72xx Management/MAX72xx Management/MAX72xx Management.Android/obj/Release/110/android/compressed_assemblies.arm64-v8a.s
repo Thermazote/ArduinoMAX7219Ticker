fb7c2b1ee0907bfcceabbb563318431250b6c111
Kobrand <aarvlg@mail.ru>
2021-05-10 11:30:58 +0300

    Android App added

---
+++ MAX72xx Management/MAX72xx Management/MAX72xx Management.Android/obj/Release/110/android/compressed_assemblies.arm64-v8a.s
@@ -0,0 +1,758 @@
+	.arch	armv8-a
+	.file	"compressed_assemblies.arm64-v8a.arm64-v8a.s"
+	.include	"compressed_assemblies.arm64-v8a-data.inc"
+
+	.section	.data.compressed_assembly_descriptors,"aw",@progbits
+	.type	.L.compressed_assembly_descriptors, @object
+	.p2align	3
+.L.compressed_assembly_descriptors:
+	/* 0: FormsViewGroup.dll */
+	/* uncompressed_file_size */
+	.word	27024
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_0
+
+	/* 1: I18N.Other.dll */
+	/* uncompressed_file_size */
+	.word	45456
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_1
+
+	/* 2: I18N.dll */
+	/* uncompressed_file_size */
+	.word	48528
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_2
+
+	/* 3: Java.Interop.dll */
+	/* uncompressed_file_size */
+	.word	208784
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_3
+
+	/* 4: MAX72xx Management.Android.dll */
+	/* uncompressed_file_size */
+	.word	302592
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_4
+
+	/* 5: MAX72xx Management.dll */
+	/* uncompressed_file_size */
+	.word	50688
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_5
+
+	/* 6: Mono.Android.dll */
+	/* uncompressed_file_size */
+	.word	29983624
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_6
+
+	/* 7: Mono.Security.dll */
+	/* uncompressed_file_size */
+	.word	251280
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_7
+
+	/* 8: Plugin.BluetoothClassic.Abstractions.dll */
+	/* uncompressed_file_size */
+	.word	16384
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_8
+
+	/* 9: Plugin.BluetoothClassic.Android.dll */
+	/* uncompressed_file_size */
+	.word	31232
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_9
+
+	/* 10: System.ComponentModel.Composition.dll */
+	/* uncompressed_file_size */
+	.word	267664
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_10
+
+	/* 11: System.Core.dll */
+	/* uncompressed_file_size */
+	.word	1094032
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_11
+
+	/* 12: System.Data.DataSetExtensions.dll */
+	/* uncompressed_file_size */
+	.word	38800
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_12
+
+	/* 13: System.Data.dll */
+	/* uncompressed_file_size */
+	.word	1935760
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_13
+
+	/* 14: System.Drawing.Common.dll */
+	/* uncompressed_file_size */
+	.word	170384
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_14
+
+	/* 15: System.IO.Compression.FileSystem.dll */
+	/* uncompressed_file_size */
+	.word	27536
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_15
+
+	/* 16: System.IO.Compression.dll */
+	/* uncompressed_file_size */
+	.word	124816
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_16
+
+	/* 17: System.Memory.dll */
+	/* uncompressed_file_size */
+	.word	13704
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_17
+
+	/* 18: System.Net.Http.dll */
+	/* uncompressed_file_size */
+	.word	291728
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_18
+
+	/* 19: System.Numerics.Vectors.dll */
+	/* uncompressed_file_size */
+	.word	21384
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_19
+
+	/* 20: System.Numerics.dll */
+	/* uncompressed_file_size */
+	.word	128904
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_20
+
+	/* 21: System.Runtime.Serialization.dll */
+	/* uncompressed_file_size */
+	.word	875912
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_21
+
+	/* 22: System.Runtime.dll */
+	/* uncompressed_file_size */
+	.word	21392
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_22
+
+	/* 23: System.ServiceModel.Internals.dll */
+	/* uncompressed_file_size */
+	.word	227728
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_23
+
+	/* 24: System.Text.Encoding.CodePages.dll */
+	/* uncompressed_file_size */
+	.word	14224
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_24
+
+	/* 25: System.Transactions.dll */
+	/* uncompressed_file_size */
+	.word	41360
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_25
+
+	/* 26: System.Web.Services.dll */
+	/* uncompressed_file_size */
+	.word	223632
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_26
+
+	/* 27: System.Xml.Linq.dll */
+	/* uncompressed_file_size */
+	.word	146832
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_27
+
+	/* 28: System.Xml.dll */
+	/* uncompressed_file_size */
+	.word	2451856
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_28
+
+	/* 29: System.dll */
+	/* uncompressed_file_size */
+	.word	1969040
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_29
+
+	/* 30: Xamarin.AndroidX.Activity.dll */
+	/* uncompressed_file_size */
+	.word	29064
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_30
+
+	/* 31: Xamarin.AndroidX.Annotation.Experimental.dll */
+	/* uncompressed_file_size */
+	.word	25472
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_31
+
+	/* 32: Xamarin.AndroidX.Annotation.dll */
+	/* uncompressed_file_size */
+	.word	149896
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_32
+
+	/* 33: Xamarin.AndroidX.AppCompat.AppCompatResources.dll */
+	/* uncompressed_file_size */
+	.word	65928
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_33
+
+	/* 34: Xamarin.AndroidX.AppCompat.dll */
+	/* uncompressed_file_size */
+	.word	1028608
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_34
+
+	/* 35: Xamarin.AndroidX.Arch.Core.Common.dll */
+	/* uncompressed_file_size */
+	.word	30600
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_35
+
+	/* 36: Xamarin.AndroidX.Arch.Core.Runtime.dll */
+	/* uncompressed_file_size */
+	.word	24448
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_36
+
+	/* 37: Xamarin.AndroidX.AsyncLayoutInflater.dll */
+	/* uncompressed_file_size */
+	.word	22408
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_37
+
+	/* 38: Xamarin.AndroidX.Browser.dll */
+	/* uncompressed_file_size */
+	.word	284552
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_38
+
+	/* 39: Xamarin.AndroidX.CardView.dll */
+	/* uncompressed_file_size */
+	.word	30088
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_39
+
+	/* 40: Xamarin.AndroidX.Collection.dll */
+	/* uncompressed_file_size */
+	.word	74120
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_40
+
+	/* 41: Xamarin.AndroidX.CoordinatorLayout.dll */
+	/* uncompressed_file_size */
+	.word	101760
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_41
+
+	/* 42: Xamarin.AndroidX.Core.dll */
+	/* uncompressed_file_size */
+	.word	1404808
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_42
+
+	/* 43: Xamarin.AndroidX.CursorAdapter.dll */
+	/* uncompressed_file_size */
+	.word	56712
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_43
+
+	/* 44: Xamarin.AndroidX.CustomView.dll */
+	/* uncompressed_file_size */
+	.word	63368
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_44
+
+	/* 45: Xamarin.AndroidX.DocumentFile.dll */
+	/* uncompressed_file_size */
+	.word	28040
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_45
+
+	/* 46: Xamarin.AndroidX.DrawerLayout.dll */
+	/* uncompressed_file_size */
+	.word	59272
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_46
+
+	/* 47: Xamarin.AndroidX.Fragment.dll */
+	/* uncompressed_file_size */
+	.word	258440
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_47
+
+	/* 48: Xamarin.AndroidX.Interpolator.dll */
+	/* uncompressed_file_size */
+	.word	21896
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_48
+
+	/* 49: Xamarin.AndroidX.Legacy.Support.Core.UI.dll */
+	/* uncompressed_file_size */
+	.word	34184
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_49
+
+	/* 50: Xamarin.AndroidX.Legacy.Support.Core.Utils.dll */
+	/* uncompressed_file_size */
+	.word	18824
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_50
+
+	/* 51: Xamarin.AndroidX.Legacy.Support.V4.dll */
+	/* uncompressed_file_size */
+	.word	14216
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_51
+
+	/* 52: Xamarin.AndroidX.Lifecycle.Common.dll */
+	/* uncompressed_file_size */
+	.word	40328
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_52
+
+	/* 53: Xamarin.AndroidX.Lifecycle.LiveData.Core.dll */
+	/* uncompressed_file_size */
+	.word	30088
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_53
+
+	/* 54: Xamarin.AndroidX.Lifecycle.LiveData.dll */
+	/* uncompressed_file_size */
+	.word	24456
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_54
+
+	/* 55: Xamarin.AndroidX.Lifecycle.Runtime.dll */
+	/* uncompressed_file_size */
+	.word	23432
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_55
+
+	/* 56: Xamarin.AndroidX.Lifecycle.ViewModel.dll */
+	/* uncompressed_file_size */
+	.word	32648
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_56
+
+	/* 57: Xamarin.AndroidX.Lifecycle.ViewModelSavedState.dll */
+	/* uncompressed_file_size */
+	.word	19848
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_57
+
+	/* 58: Xamarin.AndroidX.Loader.dll */
+	/* uncompressed_file_size */
+	.word	61832
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_58
+
+	/* 59: Xamarin.AndroidX.LocalBroadcastManager.dll */
+	/* uncompressed_file_size */
+	.word	18824
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_59
+
+	/* 60: Xamarin.AndroidX.Media.dll */
+	/* uncompressed_file_size */
+	.word	382344
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_60
+
+	/* 61: Xamarin.AndroidX.MultiDex.dll */
+	/* uncompressed_file_size */
+	.word	14216
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_61
+
+	/* 62: Xamarin.AndroidX.Print.dll */
+	/* uncompressed_file_size */
+	.word	22400
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_62
+
+	/* 63: Xamarin.AndroidX.RecyclerView.dll */
+	/* uncompressed_file_size */
+	.word	563592
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_63
+
+	/* 64: Xamarin.AndroidX.SavedState.dll */
+	/* uncompressed_file_size */
+	.word	29064
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_64
+
+	/* 65: Xamarin.AndroidX.SlidingPaneLayout.dll */
+	/* uncompressed_file_size */
+	.word	43400
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_65
+
+	/* 66: Xamarin.AndroidX.SwipeRefreshLayout.dll */
+	/* uncompressed_file_size */
+	.word	66440
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_66
+
+	/* 67: Xamarin.AndroidX.Transition.dll */
+	/* uncompressed_file_size */
+	.word	139776
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_67
+
+	/* 68: Xamarin.AndroidX.VectorDrawable.Animated.dll */
+	/* uncompressed_file_size */
+	.word	44424
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_68
+
+	/* 69: Xamarin.AndroidX.VectorDrawable.dll */
+	/* uncompressed_file_size */
+	.word	31112
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_69
+
+	/* 70: Xamarin.AndroidX.VersionedParcelable.dll */
+	/* uncompressed_file_size */
+	.word	106888
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_70
+
+	/* 71: Xamarin.AndroidX.ViewPager.dll */
+	/* uncompressed_file_size */
+	.word	83336
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_71
+
+	/* 72: Xamarin.AndroidX.ViewPager2.dll */
+	/* uncompressed_file_size */
+	.word	53128
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_72
+
+	/* 73: Xamarin.Essentials.dll */
+	/* uncompressed_file_size */
+	.word	210312
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_73
+
+	/* 74: Xamarin.Forms.Core.dll */
+	/* uncompressed_file_size */
+	.word	1222032
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_74
+
+	/* 75: Xamarin.Forms.Platform.Android.dll */
+	/* uncompressed_file_size */
+	.word	865792
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_75
+
+	/* 76: Xamarin.Forms.Platform.dll */
+	/* uncompressed_file_size */
+	.word	189328
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_76
+
+	/* 77: Xamarin.Forms.Xaml.dll */
+	/* uncompressed_file_size */
+	.word	113552
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_77
+
+	/* 78: Xamarin.Google.Android.Material.dll */
+	/* uncompressed_file_size */
+	.word	1520640
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_78
+
+	/* 79: Xamarin.Google.Guava.ListenableFuture.dll */
+	/* uncompressed_file_size */
+	.word	18072
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_79
+
+	/* 80: mscorlib.dll */
+	/* uncompressed_file_size */
+	.word	4514192
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_80
+
+	/* 81: netstandard.dll */
+	/* uncompressed_file_size */
+	.word	99704
+	/* loaded */
+	.byte	0
+	/* data */
+	.zero	3
+	.xword	compressed_assembly_data_81
+
+	.size	.L.compressed_assembly_descriptors, 1312
+	.section	.data.compressed_assemblies,"aw",@progbits
+	.type	compressed_assemblies, @object
+	.p2align	3
+	.global	compressed_assemblies
+compressed_assemblies:
+	/* count */
+	.word	82
+	/* descriptors */
+	.zero	4
+	.xword	.L.compressed_assembly_descriptors
+	.size	compressed_assemblies, 16
---
+++ MAX72xx Management/MAX72xx Management/MAX72xx Management/Settings.xaml.cs
@@ -0,0 +1,122 @@
+﻿using Plugin.BluetoothClassic.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace MAX72xx_Management
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Settings : ContentPage
+    {
+        private readonly IBluetoothAdapter _bluetoothAdapter;
+        public Settings()
+        {
+            _bluetoothAdapter = DependencyService.Resolve<IBluetoothAdapter>();
+            InitializeComponent();
+        }
+
+        private void RefreshUI()
+        {
+            if (_bluetoothAdapter.Enabled)
+            {
+                btnDisableBluetooth.IsEnabled = true;
+                btnEnableBluetooth.IsEnabled = false;
+                lvBluetoothBoundedDevices.ItemsSource = _bluetoothAdapter.BondedDevices;
+            }
+            else
+            {
+                btnDisableBluetooth.IsEnabled = false;
+                btnEnableBluetooth.IsEnabled = true;
+                lvBluetoothBoundedDevices.ItemsSource = null;
+            }
+        }
+
+        private void btnEnableBluetooth_Clicked(object sender, EventArgs args)
+        {
+            _bluetoothAdapter.Enable();
+            RefreshUI();
+        }
+
+        private void btnDisableBluetooth_Clicked(object sender, EventArgs args)
+        {
+            _bluetoothAdapter.Disable();
+            RefreshUI();
+        }
+
+        protected override async void OnAppearing()
+        {
+            RefreshUI();
+            await DisconnectIfConnectedAsync();
+        }
+
+        private async Task DisconnectIfConnectedAsync()
+        {
+            if (/*App.CurrentBluetoothConnection != null*/ Application.Current.BindingContext != null)
+            {
+                try
+                {
+                    //App.CurrentBluetoothConnection.Dispose();
+                    if (Application.Current.BindingContext is IBluetoothManagedConnection connection)
+                    {
+                        connection.Dispose();
+                    }
+                }
+                catch (Exception exception)
+                {
+                    await DisplayAlert("Error", exception.Message, "Close");
+                }
+            }
+        }
+        
+        private async void lvBluetoothBoundedDevices_ItemSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            BluetoothDeviceModel bluetoothDeviceModel = args.SelectedItem as BluetoothDeviceModel;
+            //lvBluetoothBoundedDevices.SelectedItem = null;
+            
+            if (bluetoothDeviceModel != null)
+            {
+                var connected = await TryConnect(bluetoothDeviceModel);
+                if (connected)
+                {
+                    //await Navigation.PushAsync(new MainContent());
+                }
+            }
+        }
+
+        private async Task<bool> TryConnect(BluetoothDeviceModel bluetoothDeviceModel)
+        {
+            const bool Connected = true;
+            const bool NotConnected = false;
+
+            var connection = _bluetoothAdapter.CreateManagedConnection(bluetoothDeviceModel);
+            try
+            {
+                connection.Connect();
+                Application.Current.BindingContext = connection;
+
+                return Connected;
+            }
+            catch (BluetoothConnectionException exception)
+            {
+                await DisplayAlert("Connection error", $"Can not connect to the device: {bluetoothDeviceModel.Name}({bluetoothDeviceModel.Address}).\n" +
+                    $"Exception: \"{exception.Message}\"\n" +
+                    "Please, try another one.",
+                    "Close");
+
+                    return NotConnected;
+            }
+            catch (Exception exception)
+            {
+                await DisplayAlert("Generic error", exception.Message, "Close");
+
+                return NotConnected;
+            }
+        }
+    }
+}
